ee7f0be3340d4a55dd162e94b4246d0f1781b3a1
Caio Saldanha <mrcaiosaldanha@gmail.com>
2016-04-28 10:08:22 -0300

    Código devidamente encapsulado.

--- ITA_CES22_Ex3_2_Encapsulamento/src/ITA_CES22_Ex3_2_Encapsulamento.java
+++ ITA_CES22_Ex3_2_Encapsulamento/src/ITA_CES22_Ex3_2_Encapsulamento.java
@@ -1,8 +1,27 @@
 class time{
-    int hour;
-    int minute;
+    private int hour;
+    private int minute;
     
     time() {};
+    
+    public int get_hour (){
+        return hour;
+    }
+    public int get_minute (){
+        return minute;
+    }
+    
+    public void set_hour (int hour){
+        if (hour > 23 || hour < 0)
+            System.out.println ("Valor invalido.");
+        else this.hour = hour;
+    }
+    
+    public void set_minute (int minute){
+        if (minute > 59 || minute < 0)
+            System.out.println ("Valor invalido.");
+        else this.minute = minute;
+    }
 }
 
 public class ITA_CES22_Ex3_2_Encapsulamento {
