9e43237802aead9b6c703f78bdde1d12f8af62f2
Caio Saldanha <mrcaiosaldanha@gmail.com>
2016-04-28 10:01:29 -0300

    Código inicial anterior às modificações.

---
+++ ITA_CES22_Ex3_2_Encapsulamento/src/ITA_CES22_Ex3_2_Encapsulamento.java
@@ -0,0 +1,13 @@
+class time{
+    int hour;
+    int minute;
+    
+    time() {};
+}
+
+public class ITA_CES22_Ex3_2_Encapsulamento {
+
+    public static void main(String[] args) {
+    }
+    
+}
